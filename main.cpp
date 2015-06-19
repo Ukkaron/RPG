0c6ba7eb21517f144cde8a6533eb0dee59c7181b
Ukkaron <letyaev2@gmail.com>
2015-06-19 21:56:00 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 
 using namespace std;
+class Entity
+{
+  public: int Level;
+  public: int HealthPower;
+  public: int ManaPool;
+  public: int Armor;
+  public: int Experience;
+  public: int Gold;
+  public: int WeaponID;
+  public: int[] ArtifactID = new int[6];
+  public: int X;
+  public: int Y;
+  public: Entity(int lvl, int exp, int gld, int wpnID, int[6] artID);
+};
+
+Entity::Entity(int lvl, int exp, int gld, int wpnID, int[6] artID)
+{
+    Level = lvl;
+    HealthPower = lvl * 100;
+    ManaPool = lvl * 150;
+    Experience = exp;
+    Gold = gld;
+    WeaponID = wpnID;
+    ArtifactID = artID;
+}
 
 int main()
 {
   cout << "Hellow World !" << endl;
   return 0;
 }
+
