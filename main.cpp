2c5b6de1e319212a3aff2d0a834763b0950da3df
Ziqul <mcshok.maxim@gmail.com>
2015-06-20 00:54:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,31 +3,6 @@
 #include "RPG_source.h"
 
 using namespace std;
-class Entity
-{
-  public: int Level;
-  public: int HealthPower;
-  public: int ManaPool;
-  public: int Armor;
-  public: int Experience;
-  public: int Gold;
-  public: int WeaponID;
-  public: int[] ArtifactID = new int[6];
-  public: int X;
-  public: int Y;
-  public: Entity(int lvl, int exp, int gld, int wpnID, int[6] artID);
-};
-
-Entity::Entity(int lvl, int exp, int gld, int wpnID, int[6] artID)
-{
-    Level = lvl;
-    HealthPower = lvl * 100;
-    ManaPool = lvl * 150;
-    Experience = exp;
-    Gold = gld;
-    WeaponID = wpnID;
-    ArtifactID = artID;
-}
 
 int main()
 {
