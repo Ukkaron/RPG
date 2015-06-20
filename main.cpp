166176804725866b58f8753650a6f93c006625a9
Ukkaron <letyaev2@gmail.com>
2015-06-21 02:13:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,55 +1,82 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <fstream>
 
 #include "room.h"
 #include "units.h"
-#include "weapon.h"
 
 using namespace std;
 
-void Render(cell* ptrPlatform, int rSize);
+void Render();
+void RoomPreGenerator();
+
+int rSize;
+cell platforms[16][16];
 
 int main()
 {
-    int i, j;
-    int room_size;
+    GenerateMeleeWeapons();
+    GenerateHero();
     srand(time(NULL));
-    room_size = rand() % 9 + 8;
-    cell platforms[room_size][room_size], *ptrPlatform;
-    cell *y_line[room_size];
-    for (i=0; i< room_size; i++)
-    {
-        y_line[i] = &platforms[i][0];
-    }
-    room_generator(y_line, room_size);
-    ptrPlatform = &platforms[0][0];
-    Render(ptrPlatform, room_size);
+    rSize = rand() % 9 + 8;
+    cell *ptrPlatforms;
+    ptrPlatforms = &platforms[0][0];
+    room_generator(ptrPlatforms, rSize);
+    RoomPreGenerator();
+    Render();
 }
 
-void Render(cell* ptrPlatform, int rSize)
+void Render()
 {
     int i, j;
     for(i=0; i<rSize; i++)
     {
         for(j=0; j<rSize; j++)
         {
-            if((*(ptrPlatform + i*j + j)).pit == 1)
+            if(platforms[i][j].pit == 1)
             {
                 printf("[O]");
             }
             else
             {
-                if((*(ptrPlatform + i*j + j)).rock == 1)
+                if(platforms[i][j].rock == 1)
                 {
                     printf("[W]");
                 }
                 else
                 {
-                    printf("[ ]");
+                    switch (platforms[i][j].EntityType.Type)
+                    {
+                        case HERO:
+                            printf("[H]");
+                            break;
+                        case MONSTER:
+                            printf("[A]");
+                            break;
+                        case NONE:
+                            printf("[ ]");
+                            break;
+                    }
                 }
             }
         }
         printf("\n");
     }
+}
+
+void RoomPreGenerator()
+{
+    int i;
+    int j;
+    for(i = 0; i < rSize; i++)
+    {
+        for(j = 0; j < rSize; j++)
+        {
+            platforms[i][j].EntityType.Type = NONE;
+            platforms[i][j].EntityType.EntityID = -1;
+        }
+    }
+    platforms[0][0].EntityType.Type = HERO;
+    platforms[0][0].EntityType.EntityID = 0;
 }
