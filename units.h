de1261de3e4a120b86ef5d16e58983c4589204d0
Ukkaron <letyaev2@gmail.com>
2015-06-20 13:31:52 +0300

    Update units.h

--- units.h
+++ units.h
@@ -1,3 +1,5 @@
+using namespace std;
+
 class Entity
 {
   public: int ID;
@@ -5,12 +7,12 @@ class Entity
   public: int HealthPower;
   public: int ManaPool;
   public: int Armor;
+  public: int MagicResistance;
   public: int Experience;
   public: int Gold;
   public: int WeaponID;
   public: int ArtifactID[6];
-  public: int X;
-  public: int Y;
+  public: int SkillID[4];
   public: Entity(int lvl, int exp, int gld, int wpnID, int artID[6]);
 };
 
@@ -30,4 +32,25 @@ Entity::Entity(int lvl, int exp, int gld, int wpnID, int artID[6])
             ArtifactID[i] = (*artID + i);
         }
     }
+}
+
+class Monster: public Entity
+{
+    public: int DamagePhysical;
+    public: int DamageMagic;
+    public: string Name;
+    public: void putMonster(int lvl, int strength, int exp, int gld, int DmgPh, int DmgMg, int Arm, int MgRes);
+};
+
+void Monster::putMonster(int lvl, int strength, int exp, int gld, int DmgPh, int DmgMg, int Arm, int MgRes)
+{
+    Level = lvl;
+    HealthPower = lvl*75*strength;
+    ManaPool = 0;
+    Experience = exp;
+    Gold = gld;
+    DamagePhysical = DmgPh;
+    DamageMagic = DmgMg;
+    Armor = Arm;
+    MagicResistance = MgRes;
 }
