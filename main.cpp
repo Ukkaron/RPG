3241ce52135ca41110de26c8ff1bcdc290c21104
Ukkaron <letyaev2@gmail.com>
2015-06-22 13:52:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <fstream>
+#include <ncurses.h>
 
 #include "room.h"
 #include "units.h"
@@ -16,15 +17,61 @@ cell platforms[16][16];
 
 int main()
 {
+    char Current;
+    WINDOW* CurrentWindow;
+    CurrentWindow = initscr();
+    keypad(stdscr, true);
     GenerateMeleeWeapons();
     GenerateHero();
+    printw("Press S to start");
+    do
+        Current = getch();
+    while(Current != 's');
+    clear();
     srand(time(NULL));
     rSize = rand() % 9 + 8;
     cell *ptrPlatforms;
     ptrPlatforms = &platforms[0][0];
-    room_generator(ptrPlatforms, rSize);
     RoomPreGenerator();
+    room_generator(ptrPlatforms, rSize);
     Render();
+    do
+    {
+        do
+        {
+            Current = getch();
+            switch((int)Current)
+            {
+                case 3:
+                    // code for arrow up
+                    Hero.Move(ptrPlatforms, UP);
+                    break;
+                case 2:
+                    // code for arrow down
+                    Hero.Move(ptrPlatforms, DOWN);
+                    break;
+                case 5:
+                    // code for arrow right
+                    Hero.Move(ptrPlatforms, RIGHT);
+                    break;
+                case 4:
+                    // code for arrow left
+                    Hero.Move(ptrPlatforms, LEFT);
+                    break;
+            }
+            clear();
+            Render();
+        }
+        while((int)Current != 3 || (int)Current != 2 || (int)Current != 4 || (int)Current != 5 || (int)Current != 27);
+        clear();
+        Render();
+        //spell
+        clear();
+        Render();
+        printw("You can quit!\n");
+        Current = getch();
+    }while(Current!='q');
+    endwin();
 }
 
 void Render()
@@ -36,32 +83,32 @@ void Render()
         {
             if(platforms[i][j].pit == 1)
             {
-                printf("[O]");
+                printw("[O]");
             }
             else
             {
                 if(platforms[i][j].rock == 1)
                 {
-                    printf("[W]");
+                    printw("[W]");
                 }
                 else
                 {
                     switch (platforms[i][j].EntityType.Type)
                     {
                         case HERO:
-                            printf("[H]");
+                            printw("[H]");
                             break;
                         case MONSTER:
-                            printf("[A]");
+                            printw("[A]");
                             break;
                         case NONE:
-                            printf("[ ]");
+                            printw("[ ]");
                             break;
                     }
                 }
             }
         }
-        printf("\n");
+        printw("\n");
     }
 }
 
