0e1c87e5286d888334798a7d4e01adfcfef01912
Ukkaron <letyaev2@gmail.com>
2015-06-22 13:54:23 +0300

    Update units.h

--- units.h
+++ units.h
@@ -1,28 +1,32 @@
+#include <string.h>
 #include "weapon.h"
+
+enum Arrow {UP, DOWN, LEFT, RIGHT};
 
 using namespace std;
 
 class Entity
 {
   public: int ID;
-  public: string Name;
+  public: char Name[50];
   public: int Level;
   public: int HealthPower;
   public: int Armor;
   public: int MagicResistance;
   public: int Experience;
   public: int Gold;
+  public: int x;
+  public: int y;
 };
 
-class Monster: public Entity
+class cMonster: public Entity
 {
     public: int DamagePhysical;
     public: int DamageMagic;
-    public: string Name;
     public: void putMonster(int lvl, int strength, int exp, int gld, int DmgPh, int DmgMg, int Arm, int MgRes);
 };
 
-void Monster::putMonster(int lvl, int strength, int exp, int gld, int DmgPh, int DmgMg, int Arm, int MgRes)
+void cMonster::putMonster(int lvl, int strength, int exp, int gld, int DmgPh, int DmgMg, int Arm, int MgRes)
 {
     Level = lvl;
     HealthPower = lvl*75*strength;
@@ -34,6 +38,8 @@ void Monster::putMonster(int lvl, int strength, int exp, int gld, int DmgPh, int
     MagicResistance = MgRes;
 }
 
+cMonster Monster[4];
+
 class cHero: public Entity
 {
     public: int ManaPool;
@@ -42,16 +48,19 @@ class cHero: public Entity
     public: int SkillID[4];
     public: void Show();
     public: void Save();
+    public: void Move(cell* ptrFlat, Arrow Key);
 };
 
 void cHero::Show()
 {
-    cout << "Hero " << Name << endl;
-    cout << "Level:" << Level << endl;
-    cout << "Health power: " << HealthPower << endl;
-    cout << "Mana: " << ManaPool << endl;
-    cout << "Gold: " << Gold << endl;
-    cout << "Weared weapon: " << Sword[WeaponID].Name << endl;
+    printw("Hero %s \n", Name);
+    printw("Level: %d \n", Level);
+    printw("Health power: %d \n", HealthPower);
+    printw("Mana: %d \n", ManaPool);
+    printw("Gold: %d \n", Gold);
+    char Current[50];
+    strcpy(Current, Sword[WeaponID].Name.c_str());
+    printw("Weared weapon: %s \n", Current);
 }
 void cHero::Save()
 {
@@ -66,6 +75,24 @@ void cHero::Save()
     for(i = 0; i < 6; i++)
     {
         HeroOutput << " " << SkillID[i];
+    }
+}
+void cHero::Move(cell* ptrFlat, Arrow Key)
+{
+    switch(Key)
+    {
+        case UP:
+            // code to move up
+            break;
+        case DOWN:
+            // code to move down
+            break;
+        case LEFT:
+            // code to move left
+            break;
+        case RIGHT:
+            //code to move right
+            break;
     }
 }
 
@@ -97,12 +124,12 @@ void GenerateHero()
         {
             HeroInput >> Hero.SkillID[i];
         }
-        cout << "Hero loaded." << endl;
+        printw("Hero loaded.\n");
         Hero.Show();
     }
     else
     {
-        cout << "Enter the name of your hero: ";
+        printw("Enter the name of your hero: ");
         cin >> Hero.Name;
         Hero.Experience = 1000;
         Hero.Level = Hero.Experience / 1000;
@@ -113,4 +140,31 @@ void GenerateHero()
         Hero.Gold = 1000;
     }
     HeroInput.close();
+}
+
+void MonsterGenerator()
+{
+    int lvl;
+    int strength;
+    int exp;
+    int gld;
+    int dmg;
+    int armor;
+    int magRes;
+    int i;
+    for(i = 0; i < 4; i++)
+    {
+        lvl = rand() % 10 - 5 + Hero.Level;
+        if(lvl < 1)
+        {
+            lvl = 1;
+        }
+        strength = 1;
+        exp = lvl * 1000;
+        gld = lvl * 100;
+        dmg = lvl * 50;
+        armor = lvl * 4;
+        magRes = lvl;
+        Monster[i].putMonster(lvl, strength, exp, gld, dmg, 0, armor, magRes);
+    }
 }
